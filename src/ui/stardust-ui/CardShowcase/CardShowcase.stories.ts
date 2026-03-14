96ca53b7251fcecbb548361b07c74b2a387af46c
Flicky <flicky@pollux.gg>
2026-03-14 00:21:08 -0300

    card showcase comp

--- src/ui/stardust-ui/CardShowcase/CardShowcase.stories.ts
+++ src/ui/stardust-ui/CardShowcase/CardShowcase.stories.ts
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/vue3-vite';
 import CardShowcase from './CardShowcase.vue';
 
 const meta: Meta<typeof CardShowcase> = {
-  title: 'Stardust PLX/Card Showcase',
+  title: 'Stardust UI/Misc/Card Showcase',
   component: CardShowcase,
   tags: ['autodocs'],
   argTypes: {
