04d179980383d2a16618df47ea0fabaaef2459dc
Flicky <flicky@pollux.gg>
2026-03-14 00:19:24 -0300

    feat(dashboard): add Medal component (replace MedalChip)

---
+++ src/ui/components/Medal/Medal.stories.ts
@@ -0,0 +1,44 @@
+import type { Meta, StoryObj } from '@storybook/vue3-vite';
+import Medal from './Medal.vue';
+import './Medal.scss';
+
+const meta: Meta<typeof Medal> = {
+  title: 'Pollux-Specific Components/Cosmetics/Medal',
+  component: Medal,
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Medal>;
+
+export const Basic: Story = {
+  argTypes: {
+    rarity: {
+      control: { type: 'select' },
+      options: ['UR', 'XR', 'SR', 'R', 'U', 'C', null],
+    },
+    medal: {
+      control: { type: 'text' },
+    },
+    iconPosition: {
+      control: { type: 'select' },
+      options: ['top-left', 'top-right', 'bottom-left', 'bottom-right'],
+    },
+  },
+  args: {
+    rarity: 'UR',
+    medal: 'umbrella',
+    iconPosition: 'top-left',
+  },
+  render: (args) => ({
+    components: { Medal },
+    setup() {
+      return { args };
+    },
+    template: `
+      <Medal v-bind="args" />
+    `,
+  }),
+};
+
---
+++ src/ui/components/Medal/Medal.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+div.s-medal-chip
+  div.s-medal-chip__rarity(:class="rarityIconPosition" v-if="rarity")
+    RarityIcon :rarity="rarity"
+  div.s-medal-chip__face(:style="faceStyle")
+    img(:src="`https://cdn.pollux.gg/medals/${medal}.png`")
+</template>
+
+<script setup lang="ts">
+import RarityIcon from '../RarityIcon/RarityIcon.vue';
+import { computed } from 'vue';
+import './Medal.scss';
+
+const props = defineProps<{
+  medal?: string; // URL for medal face background
+  rarity?: "UR" | "XR" | "SR" | "R" | "U"| "C";
+  iconPosition?: 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right';
+}>();
+const rarityIconPosition = computed(() =>
+  props.iconPosition
+    ? props.iconPosition
+    : 'top-left',
+);
+const faceStyle = computed(() =>
+  props.medal
+    ? `https://cdn.pollux.gg/medals/${props.medal}.png`
+    : '',
+);
+</script>
+
