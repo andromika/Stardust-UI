e9a1d39e20164eafd2d82cb71047a0d5144e30df
Flicky <flicky@pollux.gg>
2026-03-13 21:13:09 -0300

    create generic card showcase

---
+++ src/ui/stardust-ui/CardShowcase/CardShowcase.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+div.card-showcase
+  div.card-showcase__container
+    div.card-showcase__flip
+      div.card-showcase__face
+        img(
+          v-if="faceImageUrl"
+          :src="faceImageUrl"
+          :alt="faceAlt"
+          loading="lazy"
+        )
+      div.card-showcase__back
+        img(
+          v-if="backImageUrl"
+          :src="backImageUrl"
+          :alt="backAlt"
+          loading="lazy"
+        )
+</template>
+
+<script setup lang="ts">
+import './CardShowcase.scss';
+
+defineProps<{
+  /** URL for the front/face image */
+  faceImageUrl?: string;
+  /** URL for the back image */
+  backImageUrl?: string;
+  /** Alt text for the face image */
+  faceAlt?: string;
+  /** Alt text for the back image */
+  backAlt?: string;
+}>();
+</script>
