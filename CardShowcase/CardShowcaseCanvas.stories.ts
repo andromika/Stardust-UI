9ed5dcecddcbbe8f84f9eefcfc8a2945e4376fbc
Flicky <flicky@pollux.gg>
2026-03-14 21:25:04 -0300

    use canvas (wip)

--- CardShowcase/CardShowcaseCanvas.stories.ts
+++ CardShowcase/CardShowcaseCanvas.stories.ts
@@ -11,6 +11,14 @@ const meta: Meta<typeof CardShowcaseCanvas> = {
     durationMs: { control: 'number' },
     width: { control: 'number' },
     height: { control: 'number' },
+    easing: {
+      control: 'object',
+      description: 'Cubic bezier easing function defined as [x1, y1, x2, y2]',
+  },
+    rotateX: { control: { type: 'range', min: 0, max: 360, step: 1 } },
+    rotateY: { control: { type: 'range', min: 0, max: 360, step: 1 } },
+    rotateZ: { control: { type: 'range', min: 0, max: 360, step: 1 } },
+    borderRadius: { control: { type: 'range', min: 0, max: 100, step: 1 } },
   },
 };
 
@@ -20,12 +28,16 @@ type Story = StoryObj<typeof CardShowcaseCanvas>;
 
 export const Default: Story = {
   args: {
-    faceImageUrl: 'https://picsum.photos/160/180',
-    backImageUrl: 'https://picsum.photos/160/180?blur=2',
+    faceImageUrl: 'https://i.imgur.com/UEo57Di.png',
+    backImageUrl: 'https://i.imgur.com/oaL6JZ9.png',
     durationMs: 5000,
     easing: [0.68, -0.55, 0.27, 1.55],
     width: 200,
-    height: 220,
+    height: 280,
+    rotateX: 10,
+    rotateY: 15,
+    rotateZ: 0,
+    borderRadius: 16,
   },
   render: (args) => ({
     components: { CardShowcaseCanvas },
