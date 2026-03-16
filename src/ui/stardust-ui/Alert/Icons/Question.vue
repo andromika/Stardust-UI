e4883c72b0ab6e8311216cd528da6915af905578
Flicky <flicky@pollux.gg>
2026-03-16 08:50:48 -0300

    new status icons

---
+++ src/ui/stardust-ui/Alert/Icons/Question.vue
@@ -0,0 +1,18 @@
+<template>
+	<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+		<g fill="none" :stroke="fill" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5">
+			<path stroke-dasharray="60" d="M12 3c4.97 0 9 4.03 9 9c0 4.97 -4.03 9 -9 9c-4.97 0 -9 -4.03 -9 -9c0 -4.97 4.03 -9 9 -9Z">
+				<animate fill="freeze" attributeName="stroke-dashoffset" dur="0.3s" values="60;0" />
+			</path>
+			<path stroke-dasharray="18" stroke-dashoffset="18" d="M9 10c0 -1.66 1.34 -3 3 -3c1.66 0 3 1.34 3 3c0 0.98 -0.47 1.85 -1.2 2.4c-0.73 0.55 -1.3 0.6 -1.8 1.6">
+				<animate fill="freeze" attributeName="stroke-dashoffset" begin="0.35s" dur="0.15s" to="0" />
+			</path>
+			<path stroke-dasharray="4" stroke-dashoffset="4" d="M12 17v0.01">
+				<animate fill="freeze" attributeName="stroke-dashoffset" begin="0.35s" dur="0.1s" to="0" />
+			</path>
+		</g>
+	</svg>
+</template>
+<script setup lang="ts">
+const fill = getComputedStyle(document.documentElement).getPropertyValue('--plx-info') || '#3082ce';
+</script>
---
+++ src/ui/stardust-ui/Alert/Icons/Success.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+svg(xmlns="http://www.w3.org/2000/svg", width="24", height="24", viewBox="0 0 24 24")
+  g(fill="none", :stroke="fill", stroke-linecap="round", stroke-linejoin="round", stroke-width="1.8")
+    path(stroke-dasharray="60", stroke-dashoffset="60", d="M12 3c4.97 0 9 4.03 9 9s-4.03 9 -9 9s-9 -4.03 -9 -9s4.03 -9 9 -9Z")
+      animate(fill="freeze", attributeName="stroke-dashoffset", begin="0.3s", dur="0.4s", calcMode="spline", keyTimes="0;1", keySplines="0.5 0 0.5 1", values="60;15;")
+      animate(fill="freeze", attributeName="stroke-dashoffset", begin="0.63s", dur="0.3s", calcMode="spline", keyTimes="0;1", keySplines="0.5 0 0.5 1", values="15;-60;")
+    path(stroke-dasharray="60", opacity=".2", d="M12 3c4.97 0 9 4.03 9 9s-4.03 9 -9 9s-9 -4.03 -9 -9s4.03 -9 9 -9Z")
+    path(stroke-dasharray="18", stroke-dashoffset="18", d="M8 12l3 3l5 -6")
+      animate(fill="freeze", attributeName="stroke-dashoffset", begin="0.7s", dur="0.3s", calcMode="spline", keyTimes="0;1", keySplines="0.5 0 0.5 1", to="0")
+
+</template>
+<script setup lang="ts">
+const fill = getComputedStyle(document.documentElement).getPropertyValue('--plx-green') || '#31e2b4';
+</script>
