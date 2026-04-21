7f3f3204350f72d24b5dd57527dc0dcb41e43961
Flicky <flicky@pollux.gg>
2026-04-21 17:56:51 -0300

    materialize standalone project shell

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,19 +2,17 @@ import type { StorybookConfig } from '@storybook/vue3-vite';
 
 const config: StorybookConfig = {
   stories: [
-    "../src/ui/themes/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-    "../src/ui/stardust-ui/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-    "../src/ui/gravity/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-    "../src/ui/components/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    '../src/stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+    '../src/ui/themes/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+    '../src/ui/stardust-ui/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+    '../src/ui/components/**/*.stories.@(js|jsx|mjs|ts|tsx)'
   ],
-  staticDirs: ["../public"],
   addons: [
-    "@chromatic-com/storybook",
-    "@storybook/addon-a11y",
-    "@storybook/addon-docs",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-themes"
+    '@storybook/addon-a11y',
+    '@storybook/addon-docs',
+    '@storybook/addon-themes'
   ],
-  "framework": "@storybook/vue3-vite"
+  framework: '@storybook/vue3-vite'
 };
+
 export default config;
---
+++ tailwind.config.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+  content: ['./src/**/*.{vue,ts,tsx}', './.storybook/**/*.{ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+};
